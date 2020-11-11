1fad98d9e2177e0b55b65cb96bfef9cfea219d82
Malik Edwards <malikedwards777@gmail.com>
2020-11-10 23:39:45 -0500

    Exercise 3 Completed

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 "use strict";
 window.onload=function() {
-    const btn = document.getElementById('search-btn');
-    btn.onclick=function(e) {
-        console.log("button pressed");
+    const  form= document.querySelector('.search form');
+    const  btn=document.querySelector('.search button')
+
+    form.addEventListener("submit",formSubmit);
+    btn.addEventListener("click",formSubmit);
+    
+    function formSubmit(e) {
         e.preventDefault();
-        fetch("superheroes.php")
+        console.log("form submitted");
+        let message = document.querySelector(".search div.message");
+        let valid_string=/[^( \w)]+/g;
+        let input=document.querySelector(".search-group input").value;
+        
+        input=input.trim();
+        if(!(valid_string.test(input))){
+            let url = `superheroes.php?query=${input}`;
+            message.textContent="";
+            fetch(url)
             .then(response => {
                 if (response.ok){
                     return response.text();
@@ -13,8 +26,19 @@ window.onload=function() {
                 }
             })
             .then(data=> {
-                alert(data);
+                let result=document.querySelector('#result');
+                if(data!==""){
+                    result.innerHTML=data;
+                }
+                else{
+                    result.innerHTML="<h4 id='not-found'> SUPERHERO NOT FOUND</h4>";
+                }
             })
             .catch(error => console.log('There was an error: '+error));
+        }
+        else{
+            message.textContent = "Please enter a valid name. Name should not contain any special characters.";
+            message.style.color="red";
+        }//end if valid string
     }//end btn.onclick
 }//end window.onload
